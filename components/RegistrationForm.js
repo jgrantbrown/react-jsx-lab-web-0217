a18010073e0fd5202ee71cc590572f14f2b0e841
Your Name <you@example.com>
2017-04-03 16:44:42 -0400

    Done.

---
+++ components/RegistrationForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class RegistrationForm extends React.Component{
+  render (){
+    return (<form>
+    <label>Username</label>
+    <input type="text"/>
+    <label>Password</label>
+    <input type="password"/>
+    <button type="submit"/>
+    </form>)
+}
+}
